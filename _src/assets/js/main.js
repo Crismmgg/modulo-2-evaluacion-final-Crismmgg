77bc16126e12498f31ff1299956f7b38a1060265
crismmgg <cristinammgg@gmail.com>
2019-12-02 20:15:41 +0100

    local storage

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -8,6 +8,22 @@ const favContainer = document.querySelector('.js-fav-container-info');
 let series = [];
 let favSeries = [];
 
+//local storage
+
+function setLocalStorage() {
+  localStorage.setItem('favSeries', JSON.stringify(favSeries));
+}
+
+function getLocalStorage() {
+  const localStorageFavs = JSON.parse(localStorage.getItem('favSeries'));
+  if (localStorageFavs !== null) {
+    favSeries = localStorageFavs;
+    paintFavs();
+  } else {
+    getServerData();
+  }
+}
+
 //fetch
 
 function getServerData() {
@@ -20,7 +36,7 @@ function getServerData() {
       paintSeries();
       listenFavs();
       paintFavs();
-      console.log('traigo datos');
+      // console.log('traigo datos');
     })
     .catch(function(err) {
       console.log('Error al traer los datos del servidor', err);
@@ -31,16 +47,15 @@ function getServerData() {
 
 function paintSeries() {
   listContainer.innerHTML = '';
-  for (const serie of series) {
+  for (let i = 0; i < series.length; i++) {
     const serieContainer = document.createElement('li');
-    serieContainer.setAttribute('id', `${serie.show.id}`);
+    serieContainer.setAttribute('id', `${series[i].show.id}`);
     // debugger;
     const index = favSeries.findIndex(function(show, index) {
-      return show.id === clickedId;
+      return show.id === series[i].show.id;
     });
     const isFav = index !== -1;
-
-    if (isFav) {
+    if (isFav === true) {
       serieContainer.setAttribute(
         'class',
         'js-serie-container fav__serie__colors'
@@ -51,21 +66,23 @@ function paintSeries() {
     listContainer.appendChild(serieContainer);
     const title = document.createElement('h3');
     title.setAttribute('class', 'show__title');
-    const titleText = document.createTextNode(`${serie.show.name}`);
+    const titleText = document.createTextNode(`${series[i].show.name}`);
     title.appendChild(titleText);
     serieContainer.appendChild(title);
     const img = document.createElement('img');
-    if (serie.show.image === null) {
+    if (series[i].show.image === null) {
       img.setAttribute(
         'src',
         'https://via.placeholder.com/210x295/ffffff/666666/?'
       );
     } else {
-      img.setAttribute('src', `${serie.show.image.medium}`);
+      img.setAttribute('src', `${series[i].show.image.medium}`);
     }
     serieContainer.appendChild(img);
   }
+  listenFavs();
 }
+
 //señalar favoritas
 
 function addFavs(ev) {
@@ -86,35 +103,38 @@ function addFavs(ev) {
   }
   console.log(index);
   console.log(favSeries);
+  setLocalStorage();
+  paintFavs();
   listenFavs();
+  paintSeries();
 }
+
+//pintar favs
 
 function paintFavs() {
   favContainer.innerHTML = '';
   for (let i = 0; i < favSeries.length; i++) {
-    for (const serie of series) {
-      const serieContainer = document.createElement('li');
-      serieContainer.setAttribute('id', `${serie.show.id}`);
-      serieContainer.setAttribute('class', 'js-serie-container');
-      listContainer.appendChild(serieContainer);
-      const title = document.createElement('h3');
-      title.setAttribute('class', 'show__title');
-      const titleText = document.createTextNode(`${serie.show.name}`);
-      title.appendChild(titleText);
-      serieContainer.appendChild(title);
-      const img = document.createElement('img');
-      if (serie.show.image === null) {
-        img.setAttribute(
-          'src',
-          'https://via.placeholder.com/210x295/ffffff/666666/?'
-        );
-      } else {
-        img.setAttribute('src', `${serie.show.image.medium}`);
-      }
-      serieContainer.appendChild(img);
+    // debugger;
+    const serieContainer = document.createElement('li');
+    favContainer.appendChild(serieContainer);
+    const title = document.createElement('h3');
+    title.setAttribute('class', 'show__title');
+    const titleText = document.createTextNode(`${favSeries[i].name}`);
+    title.appendChild(titleText);
+    serieContainer.appendChild(title);
+    const img = document.createElement('img');
+    if (favSeries[i].image === null) {
+      img.setAttribute(
+        'src',
+        'https://via.placeholder.com/210x295/ffffff/666666/?'
+      );
+    } else {
+      img.setAttribute('src', `${favSeries[i].image.medium}`);
     }
+    serieContainer.appendChild(img);
   }
 }
+
 function listenFavs() {
   const serieItems = document.querySelectorAll('.js-serie-container');
   for (const serieItem of serieItems) {
@@ -122,4 +142,10 @@ function listenFavs() {
   }
 }
 
-button.addEventListener('click', getServerData);
+function handler(ev) {
+  ev.preventDefault();
+  getServerData();
+}
+
+button.addEventListener('click', handler);
+getLocalStorage();
