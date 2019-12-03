38119a85fee0a42ca97c8225289eac1efa0edf86
crismmgg <cristinammgg@gmail.com>
2019-12-03 20:32:17 +0100

    quotes

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,24 +1,24 @@
 /* eslint-disable no-console */
-'use strict';
+"use strict";
 
 //CONSTANTES
 
-const inputSearch = document.querySelector('.js-input');
-const listContainer = document.querySelector('.js-list-container-info');
-const button = document.querySelector('.js-button');
-const favContainer = document.querySelector('.js-fav-container-info');
-const resetButton = document.querySelector('.js-reset-button');
+const inputSearch = document.querySelector(".js-input");
+const listContainer = document.querySelector(".js-list-container-info");
+const button = document.querySelector(".js-button");
+const favContainer = document.querySelector(".js-fav-container-info");
+const resetButton = document.querySelector(".js-reset-button");
 let series = [];
 let favSeries = [];
 
 //LOCAL STORAGE
 
 function setLocalStorage() {
-  localStorage.setItem('favSeries', JSON.stringify(favSeries));
+  localStorage.setItem("favSeries", JSON.stringify(favSeries));
 }
 
 function getLocalStorage() {
-  const localStorageFavs = JSON.parse(localStorage.getItem('favSeries'));
+  const localStorageFavs = JSON.parse(localStorage.getItem("favSeries"));
   if (localStorageFavs !== null) {
     favSeries = localStorageFavs;
     paintFavs();
@@ -41,44 +41,44 @@ function getServerData() {
       paintFavs();
     })
     .catch(function(err) {
-      console.log('Error al traer los datos del servidor', err);
+      console.log("Error al traer los datos del servidor", err);
     });
 }
 
 //PINTAR SERIES
 
 function paintSeries() {
-  listContainer.innerHTML = '';
+  listContainer.innerHTML = "";
   for (let i = 0; i < series.length; i++) {
-    const serieContainer = document.createElement('li');
-    serieContainer.setAttribute('id', `${series[i].show.id}`);
+    const serieContainer = document.createElement("li");
+    serieContainer.setAttribute("id", `${series[i].show.id}`);
     const index = favSeries.findIndex(function(show) {
       return show.id === series[i].show.id;
     });
     const isFav = index !== -1;
     if (isFav === true) {
       serieContainer.setAttribute(
-        'class',
-        'js-serie-container fav__serie__colors'
+        "class",
+        "js-serie-container fav__serie__colors"
       );
     } else {
-      serieContainer.setAttribute('class', 'js-serie-container');
+      serieContainer.setAttribute("class", "js-serie-container");
     }
     listContainer.appendChild(serieContainer);
 
-    const title = document.createElement('h3');
+    const title = document.createElement("h3");
     const titleText = document.createTextNode(`${series[i].show.name}`);
     title.appendChild(titleText);
     serieContainer.appendChild(title);
 
-    const img = document.createElement('img');
+    const img = document.createElement("img");
     if (series[i].show.image === null) {
       img.setAttribute(
-        'src',
-        'https://via.placeholder.com/210x295/ffffff/666666/?'
+        "src",
+        "https://via.placeholder.com/210x295/ffffff/666666/?"
       );
     } else {
-      img.setAttribute('src', `${series[i].show.image.medium}`);
+      img.setAttribute("src", `${series[i].show.image.medium}`);
     }
     serieContainer.appendChild(img);
   }
@@ -115,44 +115,44 @@ function addFavs(ev) {
 //pintar favs
 
 function paintFavs() {
-  favContainer.innerHTML = '';
+  favContainer.innerHTML = "";
   for (let i = 0; i < favSeries.length; i++) {
     // debugger;
-    const serieContainer = document.createElement('li');
-    serieContainer.setAttribute('id', `${favSeries[i].id}`);
+    const serieContainer = document.createElement("li");
+    serieContainer.setAttribute("id", `${favSeries[i].id}`);
     serieContainer.setAttribute(
-      'class',
-      'js-serie-container fav__serie__colors icon'
+      "class",
+      "js-serie-container fav__serie__colors icon"
     );
     favContainer.appendChild(serieContainer);
 
-    const title = document.createElement('h3');
-    title.setAttribute('class', 'fav__title');
+    const title = document.createElement("h3");
+    title.setAttribute("class", "fav__title");
     const titleText = document.createTextNode(`${favSeries[i].name}`);
     title.appendChild(titleText);
     serieContainer.appendChild(title);
 
-    const img = document.createElement('img');
-    img.setAttribute('class', 'fav__img');
+    const img = document.createElement("img");
+    img.setAttribute("class", "fav__img");
     if (favSeries[i].image === null) {
       img.setAttribute(
-        'src',
-        'https://via.placeholder.com/210x295/ffffff/666666/?'
+        "src",
+        "https://via.placeholder.com/210x295/ffffff/666666/?"
       );
     } else {
-      img.setAttribute('src', `${favSeries[i].image.medium}`);
+      img.setAttribute("src", `${favSeries[i].image.medium}`);
     }
     serieContainer.appendChild(img);
-    resetButton.classList.remove('hidden');
+    resetButton.classList.remove("hidden");
   }
 }
 
 //función listener favs
 
 function listenFavs() {
-  const serieItems = document.querySelectorAll('.js-serie-container');
+  const serieItems = document.querySelectorAll(".js-serie-container");
   for (const serieItem of serieItems) {
-    serieItem.addEventListener('click', addFavs);
+    serieItem.addEventListener("click", addFavs);
   }
 }
 
@@ -161,20 +161,20 @@ function listenFavs() {
 function resetFav(ev) {
   ev.preventDefault();
   favSeries.splice(0, favSeries.length);
-  resetButton.classList.add('hidden');
-  localStorage.removeItem('favSeries');
+  resetButton.classList.add("hidden");
+  localStorage.removeItem("favSeries");
   paintFavs();
   paintSeries();
 }
 
-resetButton.addEventListener('click', resetFav);
+resetButton.addEventListener("click", resetFav);
 
 //REMOVE FAV
 
 function listenerRemoveFav() {
-  const favItems = document.querySelectorAll('.fav__serie__colors');
+  const favItems = document.querySelectorAll(".fav__serie__colors");
   for (const favItem of favItems) {
-    favItem.addEventListener('click', removeFav);
+    favItem.addEventListener("click", removeFav);
   }
 }
 
@@ -196,5 +196,5 @@ function handler(ev) {
   getServerData();
 }
 
-button.addEventListener('click', handler);
+button.addEventListener("click", handler);
 getLocalStorage();
